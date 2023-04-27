95d539d491b4bf4ded5352dbfacfae6dcc0f7ed7
Vasudev Patel <vasudev.patel@sap.com>
2023-04-26 17:36:36 -0700

    remove spcific rules UI

--- src/App.js
+++ src/App.js
@@ -3,8 +3,6 @@ import { useState, useEffect, useCallback } from 'react';
 import './App.css';
 // components
 import {
-  Button,
-  Stack,
   Navbar,
   Container,
   Toast,
@@ -14,20 +12,9 @@ import {
 } from 'react-bootstrap';
 
 // custom compos
-import { URLInput } from './URLInput';
 import { useShowToast } from './utils/hooks/useShowToast';
 
 function App() {
-  const [baseUrls, setBaseUrls] = useState({
-    devPortalBaseUrl: '',
-    argoBaseUrl: '',
-    jiraBaseUrl: '',
-    jiraMyBoardUrl: '',
-    jiraBoardUrl: '',
-    jiraBacklogUrl: '',
-    jenkinsBaseUrl: '',
-  });
-
   // {command:'', url: ''}
   const [rules, setRules] = useState([]);
   const [newRule, setNewRule] = useState({ command: '', url: '' });
@@ -37,16 +24,8 @@ function App() {
   } = useShowToast(false, 3000);
 
   useEffect(async () => {
-    const baseUrls =
-      (await chrome.storage.local.get(['baseUrls'])).baseUrls || {};
-    setBaseUrls(baseUrls);
-
     const rules = (await chrome.storage.local.get(['rules'])).rules || [];
     setRules(rules);
-  }, []);
-
-  const handleChange = useCallback((name, value) => {
-    setBaseUrls((baseUrls) => ({ ...baseUrls, [name]: value }));
   }, []);
 
   const handleRuleUrlChange = useCallback((command, newUrl) => {
@@ -68,59 +47,7 @@ function App() {
         </Container>
       </Navbar>
       <Tabs className="mt-1">
-        <Tab title={'Predefined Rules'} eventKey={'predefined'}>
-          <Stack gap={2} className={'mt-5'}>
-            <Stack gap={1}>
-              <URLInput
-                label={'Dev Portal Base URL'}
-                value={baseUrls['devPortalBaseUrl']}
-                onChange={(e) =>
-                  handleChange('devPortalBaseUrl', e.target.value)
-                }
-              />
-              <URLInput
-                label={'Jira Base URL'}
-                value={baseUrls['jiraBaseUrl']}
-                onChange={(e) => handleChange('jiraBaseUrl', e.target.value)}
-              />
-              <URLInput
-                label={'Argo Base URL'}
-                value={baseUrls['argoBaseUrl']}
-                onChange={(e) => handleChange('argoBaseUrl', e.target.value)}
-              />
-              <URLInput
-                label={'Jira Myboard URL'}
-                value={baseUrls['jiraMyBoardUrl']}
-                onChange={(e) => handleChange('jiraMyBoardUrl', e.target.value)}
-              />
-              <URLInput
-                label={'Jira Board URL'}
-                value={baseUrls['jiraBoardUrl']}
-                onChange={(e) => handleChange('jiraBoardUrl', e.target.value)}
-              />
-              <URLInput
-                label={'Jira Backlog URL'}
-                value={baseUrls['jiraBacklogUrl']}
-                onChange={(e) => handleChange('jiraBacklogUrl', e.target.value)}
-              />
-              <URLInput
-                label={'Jenkins Base URL'}
-                value={baseUrls['jenkinsBaseUrl']}
-                onChange={(e) => handleChange('jenkinsBaseUrl', e.target.value)}
-              />
-              <Button
-                onClick={() => {
-                  chrome.storage.local.set({ baseUrls: baseUrls });
-                  showSuccesfulSaveToast();
-                }}
-                className={'mt-3'}
-              >
-                Save
-              </Button>
-            </Stack>
-          </Stack>
-        </Tab>
-        <Tab title={'Custom Rules'} eventKey={'custom'}>
+        <Tab title={'Rules'} eventKey={'custom'}>
           <div className="mt-5">
             <div>
               {rules.map((rule) => (
