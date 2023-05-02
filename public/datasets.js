79902a8fdb6dfae6cdb5088f5abf6c57aa1d219c
Vasudev Patel <vasudev.patel@sap.com>
2023-05-02 14:03:29 -0700

    change premade datasets organization

---
+++ public/datasets.js
@@ -0,0 +1,306 @@
+export const datasets = {
+  repositories: [
+    {
+      name: 'agreement-masterdata',
+      org: 'WDF',
+    },
+    {
+      name: 'agreement-search',
+      org: 'WDF',
+    },
+    {
+      name: 'analytics-dashboard-ui',
+      org: 'WDF',
+    },
+    {
+      name: 'anti-virus',
+      org: 'WDF',
+    },
+    {
+      name: 'api-management',
+      org: 'TOOLS',
+    },
+    {
+      name: 'api-management-ui',
+      org: 'TOOLS',
+    },
+    {
+      name: 'approval',
+      org: 'WDF',
+    },
+    {
+      name: 'approval-ui',
+      org: 'WDF',
+    },
+    {
+      name: 'ar-open-item',
+      org: 'TOOLS',
+    },
+    {
+      name: 'aropenitem-ui',
+      org: 'TOOLS',
+    },
+    {
+      name: 'attachment-ng',
+      org: 'TOOLS',
+    },
+    {
+      name: 'biz-install',
+      org: 'TOOLS',
+    },
+    {
+      name: 'btp-state-management',
+      org: 'TOOLS',
+    },
+    {
+      name: 'business-support-system',
+      org: 'TOOLS',
+    },
+    {
+      name: 'claims-backend',
+      org: 'WDF',
+    },
+    {
+      name: 'claims-settlement',
+      org: 'TOOLS',
+    },
+    {
+      name: 'claims-submission-ui',
+      org: 'WDF',
+    },
+    {
+      name: 'comments',
+      org: 'WDF',
+    },
+    {
+      name: 'comment-ui',
+      org: 'WDF',
+    },
+    {
+      name: 'company-configuration',
+      org: 'TOOLS',
+    },
+    {
+      name: 'company-ui',
+      org: 'TOOLS',
+    },
+    {
+      name: 'conductor',
+      org: 'TOOLS',
+    },
+    {
+      name: 'configuration-ui',
+      org: 'TOOLS',
+    },
+    {
+      name: 'customer-masterdata',
+      org: 'TOOLS',
+    },
+    {
+      name: 'data-initializer-ng',
+      org: 'WDF',
+    },
+    {
+      name: 'demo-data-ui',
+      org: 'WDF',
+    },
+    {
+      name: 'dn-line-aggregation-ng',
+      org: 'WDF',
+    },
+    {
+      name: 'document-library',
+      org: 'WDF',
+    },
+    {
+      name: 'document-library-ui',
+      org: 'WDF',
+    },
+    {
+      name: 'echo',
+      org: 'WDF',
+    },
+    {
+      name: 'elastic-apm-info-service',
+      org: 'WDF',
+    },
+    {
+      name: 'eureka-admin-shell-ui',
+      org: 'TOOLS',
+    },
+    {
+      name: 'eureka-scp-integration',
+      org: 'TOOLS',
+    },
+    {
+      name: 'eureka-shell-ui',
+      org: 'TOOLS',
+    },
+    {
+      name: 'excelextraction',
+      org: 'WDF',
+    },
+    {
+      name: 'excelextraction-ui',
+      org: 'WDF',
+    },
+    {
+      name: 'ff-agent',
+      org: 'TOOLS',
+    },
+    {
+      name: 'fragments-ui',
+      org: 'WDF',
+    },
+    {
+      name: 'hierarchy-masterdata',
+      org: 'WDF',
+    },
+    {
+      name: 'integration-log',
+      org: 'TOOLS',
+    },
+    {
+      name: 'integration-logs-ui',
+      org: 'TOOLS',
+    },
+    {
+      name: 'internal-usage-metering',
+      org: 'WDF',
+    },
+    {
+      name: 'invoice-backend',
+      org: 'WDF',
+    },
+    {
+      name: 'knative-proxy',
+      org: 'WDF',
+    },
+    {
+      name: 'kraken-alerts',
+      org: 'WDF',
+    },
+    {
+      name: 'kraken-scheduler-ui',
+      org: 'WDF',
+    },
+    {
+      name: 'login-ui',
+      org: 'TOOLS',
+    },
+    {
+      name: 'metering',
+      org: 'TOOLS',
+    },
+    {
+      name: 'notification-push',
+      org: 'TOOLS',
+    },
+    {
+      name: 'pdf-extraction',
+      org: 'WDF',
+    },
+    {
+      name: 'pdf-extraction-ui',
+      org: 'WDF',
+    },
+    {
+      name: 'product-masterdata-service',
+      org: 'WDF',
+    },
+    {
+      name: 'product-md-validation-ng',
+      org: 'WDF',
+    },
+    {
+      name: 'promotion-masterdata',
+      org: 'WDF',
+    },
+    {
+      name: 'python-sample-project',
+      org: 'WDF',
+    },
+    {
+      name: 'recommendation-configuration-ui',
+      org: 'WDF',
+    },
+    {
+      name: 'recommend-promotions',
+      org: 'WDF',
+    },
+    {
+      name: 'responsibility-management',
+      org: 'TOOLS',
+    },
+    {
+      name: 'role-permission',
+      org: 'TOOLS',
+    },
+    {
+      name: 'sales-org',
+      org: 'WDF',
+    },
+    {
+      name: 'settlement-ui',
+      org: 'TOOLS',
+    },
+    {
+      name: 'subaccount',
+      org: 'WDF',
+    },
+    {
+      name: 'tenant-lcm',
+      org: 'TOOLS',
+    },
+    {
+      name: 'tenant-subscriber',
+      org: 'WDF',
+    },
+    {
+      name: 'uom-validator-converter',
+      org: 'WDF',
+    },
+    {
+      name: 'user-configuration',
+      org: 'TOOLS',
+    },
+    {
+      name: 'user-ui',
+      org: 'TOOLS',
+    },
+  ],
+  environments: [
+    {
+      cluster: 'azure-eu1-itcm-dev',
+      namespace: 'dev',
+    },
+    {
+      cluster: 'azure-eu1-itcm-stage',
+      namespace: 'staging',
+    },
+    {
+      cluster: 'sap-itcm-stage-us',
+      namespace: 'stage',
+    },
+    {
+      cluster: 'sap-itcm-prod-us',
+      namespace: 'prod',
+    },
+    {
+      cluster: 'itcm-dev',
+      namespace: 'dev',
+    },
+    {
+      cluster: 'prod',
+      namespace: 'prod',
+    },
+    {
+      cluster: 'staging-1',
+      namespace: 'staging',
+    },
+    {
+      cluster: 'trial-1',
+      namespace: 'test-drive',
+    },
+  ],
+};
