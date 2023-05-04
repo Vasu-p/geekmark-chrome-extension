1ff37ad6a9ae14e603cb8fa71b60252c648dd662
Vasudev Patel <vasudev.patel@sap.com>
2023-05-04 13:58:38 -0700

    add mock datasets

---
+++ src/mock/datasets.js
@@ -0,0 +1,20 @@
+export const datasets = [
+  {
+    name: 'repositories',
+    values: [
+      { name: 'c-ui' },
+      { name: 'c-ba' },
+      { name: 'd-ui' },
+      { name: 'd-ba' },
+    ],
+  },
+  {
+    name: 'environments',
+    values: [
+      { name: 'dev' },
+      { name: 'staging' },
+      { name: 'prod' },
+      { name: 'trial' },
+    ],
+  },
+];
