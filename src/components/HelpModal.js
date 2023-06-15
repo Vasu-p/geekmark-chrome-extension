35f17bc101836236fa38b229f254f60dc058015b
Vasudev Patel <vasudev.patel@sap.com>
2023-06-14 18:56:25 -0700

    change content

--- src/components/HelpModal.js
+++ src/components/HelpModal.js
@@ -1,54 +1,125 @@
 import React from 'react';
-import { Modal, Card, ListGroup, ListGroupItem } from 'react-bootstrap';
+import { Modal } from 'react-bootstrap';
 
 export function HelpModal({ show, onClose }) {
   return (
-    <Modal show={show} onHide={onClose}>
+    <Modal show={show} onHide={onClose} size="lg">
       <Modal.Header closeButton>
         <Modal.Title>How to use</Modal.Title>
       </Modal.Header>
       <Modal.Body>
-        <Card>
-          <Card.Body>
-            <Card.Title>Simple Rules</Card.Title>
-            <Card.Text>
-              To create a simple rule, use the following format:
-              <br />
-              <code>command:: shortcut URL:: url</code>
-              <br />
-              For example:
-              <br />
-              <code>command:: g URL:: https://www.google.com</code>
-              <br />
-              This will create a shortcut for going to Google.
-            </Card.Text>
-            <Card.Title>Rules with Parameters</Card.Title>
-            <Card.Text>
-              To create a rule with a parameter, use the following format:
-              <br />
-              <code>
-                command:: shortcut &#123;&#123;param&#125;&#125; URL::
-                url&#123;&#123;param&#125;&#125;
-              </code>
-              <br />
-              For example:
-              <br />
-              <code>
-                command:: mail &#123;&#123;folder&#125;&#125; URL::
-                https://mail.google.com/mail/u/0/#&#123;&#123;folder&#125;&#125;
-              </code>
-              <br />
-              This will create a shortcut for going to a specific folder in
-              Gmail. To use it, type "mail foldername" (e.g. "mail inbox").
-            </Card.Text>
-            <Card.Text>
-              Note: Only one parameter is supported at this time, and it must be
-              at the end of the command.
-            </Card.Text>
-          </Card.Body>
-        </Card>
+        The Geekmark bookmark extension allows users to specify bookmark rules
+        with commands and URLs. These rules provide a convenient way to navigate
+        to specific URLs by typing commands in the browser's address bar.
+        <h2>Rule Format</h2>
+        Each rule consists of two parts: a command and a URL. The command is
+        what the user types in the address bar, and the URL is the corresponding
+        web address to navigate to. The extension supports simple rules as well
+        as rules with parameters.
+        <h3>Simple Rules</h3>
+        Simple rules are straightforward mappings between a command and a URL.
+        When the user types the command, the extension will open the specified
+        URL.
+        <h4>Rule Format</h4>
+        {/* prettier-ignore */}
+        <pre>
+          command:: [command]
+          URL:: [URL]
+        </pre>
+        <h4>Example:</h4>
+        {/* prettier-ignore */}
+        <pre>
+          command:: g
+          URL:: https://www.google.com
+        </pre>
+        In this example, when the user types "g" in the address bar and presses
+        Enter, the extension will open "https://www.google.com".
+        <h3>Rules with Parameters</h3>
+        Rules with parameters allow users to specify dynamic parts in the URL.
+        The parameter acts as a placeholder that can be replaced with specific
+        values when using the command. The parameter must be the last part of
+        the command and can be placed anywhere in the URL.
+        <h4>Rule Format:</h4>
+        {/* prettier-ignore */}
+        <pre>
+          command:: [command] {'{{param}}'}
+          URL:: [URL with {'{{param}}'} placeholder]
+        </pre>
+        <h4>Example:</h4>
+        {/* prettier-ignore */}
+        <pre>
+          command:: mail {'{{param}}'}
+          URL:: https://mail.google.com/mail/u/0/#{'{{param}}'}
+        </pre>
+        In this example, the user can type "mail inbox" in the address bar, and
+        the extension will open "https://mail.google.com/mail/u/0/#inbox".
+        Similarly, typing "mail sent" will open
+        "https://mail.google.com/mail/u/0/#sent".
+        <h3>Limitations</h3>
+        <ul>
+          <li>Only one parameter is supported per rule.</li>
+          <li>The parameter must be the last part of the command.</li>
+          <li>The parameter can be placed anywhere in the URL.</li>
+        </ul>
+        <h2>Usage</h2>
+        To use the Geekmark bookmark extension, follow these steps:
+        <ol>
+          <li>Install the Geekmark extension in your browser.</li>
+          <li>Open the extension settings or options.</li>
+          <li>
+            Add a new bookmark rule by specifying the command and URL.
+            <ul>
+              <li>
+                For simple rules, enter the command and the corresponding URL.
+              </li>
+              <li>
+                For rules with parameters, enter the command with the parameter
+                placeholder and the URL with the {'{{param}}'} placeholder.
+              </li>
+            </ul>
+          </li>
+          <li>Save the bookmark rule.</li>
+          <li>
+            To use a bookmark rule, type the command in the browser's address
+            bar and press Enter.
+          </li>
+        </ol>
       </Modal.Body>
     </Modal>
   );
 }
+
+/*
+Using React Boostrap https://react-bootstrap.github.io/ write help component in React for a bookmark extension named Geekmark which helps user to specify bookmark rules.
+
+Each rule has two parts: command and URL.
+
+Users can specify simple rules such as
+command:: g
+URL:: https://www.google.com
+
+command:: mail
+URL:: https://mail.google.com/mail/u/0/#inbox
+
+Users can specify rules with parameters. For example,
+command:: mail {{param}}
+URL:: https://mail.google.com/mail/u/0/#{{param}}
+if user types "mail inbox" it will go to "https://mail.google.com/mail/u/0/#inbox"
+if user types "mail sent" it will go to "https://mail.google.com/mail/u/0/#sent"
+
+another example
+command:: laxto {{airport}}
+URL:: https://www.skyscanner.com/transport/flights/lax/{{airport}}?oym=2307&selectedoday=01&iym=2307&selectediday=01
+if user types "laxto bna" it will go to "https://www.skyscanner.com/transport/flights/lax/bna?oym=2307&selectedoday=01&iym=2307&selectediday=01"
+if user types "laxto san" it will go to "https://www.skyscanner.com/transport/flights/lax/san?oym=2307&selectedoday=01&iym=2307&selectediday=01"
+
+The parameter name doesnt matter (param, airport in above examples). They should just match in command and URL. The param must be at last in command but it can be anywhere in URL. Only 1 param is supported at this moment.
+
+Constraints:
+
+- Do not use Row, Col, Container, Accordion components
+- Use <kbd> html tag for command
+- Use <code> html tag for URL
+*/
+
 // {'{{param}}'}
