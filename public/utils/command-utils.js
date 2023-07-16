9d4117cc3c70a882ccb9984406892f4e98c85bd0
Patel Vasudev <vasudevp_24@yahoo.in>
2023-07-15 21:57:47 -0700

    Add ability to have objects as part of dataset and use it to make complex rules (#3)
    * wip
    
    * fix param passing
    
    * remove not necessary variable
    
    * fix bug
    
    * implement generateUrlForAdvancedRuleWithNestedParam
    
    * update generateUrlForAdvancedRuleWithNestedParam
    
    * pass parameters to function
    
    * update get dataset function
    
    * use replace instead of replaceAll
    
    * fix bug
    
    * add unit test
    
    ---------
    
    Co-authored-by: Vasudev Patel <vasudev.patel@sap.com>

--- public/utils/command-utils.js
+++ public/utils/command-utils.js
@@ -1,6 +1,7 @@
 import { get_closest_match } from './string-utils.js';
 
 export const paramRegex = /{{(.*?)}}/g;
+export const bracesRegex = /{{|}}/g;
 
 export function matches(typedCommand, ruleCommand) {
   const ruleRegex = generateRuleRegex(ruleCommand);
@@ -32,38 +33,33 @@ export function generateUrlForAdvancedRule(typedCommand, rule, dataset) {
   // advance rule doesnt substitute the user typed param directly in rule url (as simple rule does)
   // in advance rule, we find the closest string in dataset which matches the user typed param and substitute that
 
-  // typedCommand "abcxyz def"
-  const command = rule.command; // "abcxyz {param}"
-  const url = rule.url; // "replacement {param}""
-  const param = rule.command.match(paramRegex)[0];
+  const command = rule.command;
+  const commandParam = getCommandParam(command); // "{{param}}"
 
-  // find position of param in command
-  const commandParamPosition = command.indexOf(param);
-  // find string at above position in typedCommand
-  const paramValue = typedCommand.substr(commandParamPosition, param.length);
-  // replace url param with found string
-  return url.replace(
-    paramRegex,
-    get_closest_match(paramValue, dataset.values, (record) => record)
-  );
+  if (commandParam.includes('.')) {
+    return generateUrlForAdvancedRuleWithNestedParam(
+      typedCommand,
+      rule,
+      dataset
+    );
+  } else {
+    return generateUrlForAdvancedRuleWithSimpleParam(
+      typedCommand,
+      rule,
+      dataset
+    );
+  }
 }
 
 export function getMatchingDataset(datasets, rule) {
-  const param = rule.command.match(paramRegex)[0];
-  const paramWithoutBraces = param.replaceAll('{', '').replaceAll('}', '');
+  const commandParam = rule.command.match(paramRegex)[0];
+  const commandParamWithoutBraces = commandParam.replace(bracesRegex, '');
 
-  console.log(
-    'param',
-    param,
-    'param without braces',
-    paramWithoutBraces,
-    'rule',
-    rule
-  );
+  const datasetFromRule = isRuleWithNestedParam(rule)
+    ? commandParamWithoutBraces.split('.')[0]
+    : commandParamWithoutBraces;
 
-  return datasets.filter(
-    (dataset) => dataset.shortName === paramWithoutBraces
-  )[0];
+  return datasets.filter((dataset) => dataset.shortName === datasetFromRule)[0];
 }
 
 function generateRuleRegex(str) {
@@ -72,4 +68,64 @@ function generateRuleRegex(str) {
   return found
     ? new RegExp(str.replace(found[0], '[a-zA-Z-0-9]+'), 'g')
     : undefined;
+}
+
+function getCommandParam(command) {
+  const found = command.match(paramRegex);
+  return found ? found[0] : undefined;
+}
+
+function isRuleWithNestedParam(rule) {
+  const commandParam = getCommandParam(rule.command);
+  return commandParam.includes('.');
+}
+
+export function generateUrlForAdvancedRuleWithNestedParam(
+  typedCommand,
+  rule,
+  dataset
+) {
+  const commandParam = getCommandParam(rule.command); // {{repo.name}}
+  const commandParamWithoutBraces = commandParam.replace(bracesRegex, ''); // repo.name
+  const commandParamAccessor = commandParamWithoutBraces.split('.')[1]; // 'name'
+  // find position of param in command
+  const commandParamPosition = rule.command.indexOf(commandParam); // index of {{repo.name}} in command
+  // find string at above position in typedCommand
+  const commandParamValue = typedCommand.substr(commandParamPosition).trim(); // "name" value to fuzzy search for in dataset
+  // e.g. rule url with more than 1 params = "https://github.com/repos/{{repo.owner}}/{{repo.name}}/issues"
+
+  const foundDataset = get_closest_match(
+    commandParamValue,
+    dataset.values,
+    (record) => record[commandParamAccessor]
+  ); // { name: 'abc', owner: 'def' }
+
+  // find all params in rule url
+  const urlParams = rule.url.match(paramRegex); // ['{{repo.owner}}', '{{repo.name}}']
+  // replace all params in rule url with fuzzy searched values
+  const url = urlParams.reduce((acc, param) => {
+    const paramWithoutBraces = param.replace(bracesRegex, ''); // 'repo.owner'
+    const paramAccessor = paramWithoutBraces.split('.')[1]; // 'owner'
+    const paramValue = foundDataset[paramAccessor]; // 'def'
+    return acc.replace(param, paramValue);
+  }, rule.url);
+
+  return url;
+}
+
+function generateUrlForAdvancedRuleWithSimpleParam(
+  typedCommand, // typedCommand "abcxyz def"
+  rule,
+  dataset // { values: ['abc', 'def', 'ghi'], shortName: 'repo', name: 'repositories' }
+) {
+  const param = getCommandParam(rule.command);
+  // find position of param in command
+  const commandParamPosition = rule.command.indexOf(param);
+  // find string at above position in typedCommand
+  const paramValue = typedCommand.substr(commandParamPosition).trim();
+  // replace url param with found string
+  return rule.url.replace(
+    paramRegex,
+    get_closest_match(paramValue, dataset.values, (record) => record)
+  );
 }
