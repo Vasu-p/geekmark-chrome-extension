f957085e4220febcc2eaa1f7fe2d512b11a922cb
Vasudev Patel <vasudev.patel@sap.com>
2023-08-06 22:08:23 -0700

    add tests for matches method

--- src/tests/command-utils.test.js
+++ src/tests/command-utils.test.js
@@ -30,6 +30,10 @@ describe('matches', () => {
     ['abcdef', 'dac{{param}}', false],
     ['ab typedCommand', 'ab {{par}}', true],
     ['abcd typedCommand', 'ab {{par}}', false],
+    ['command ab cd ef', 'command {{p1}} {{par2}} {{param3}}', true],
+    ['command ab cd ef', 'comman {{p1}} {{par2}} {{param3}}', false],
+    ['command ab cd ef', 'command {{p1}} {{par2.x}} {{param3.y}}', true],
+    ['command ab cd ef', 'comman {{p1}} {{par2}} {{param3}}', false],
   ])('Works for %s', (typedCommand, ruleCommand, expected) => {
     expect(matches(typedCommand, ruleCommand)).toBe(expected);
   });
