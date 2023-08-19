123ff4f4745996abf500e97ebc9fc3fb8ade48a9
Vasudev Patel <vasudev.patel@sap.com>
2023-08-19 16:47:30 -0700

    remove outdated tests

--- src/tests/tests.test.js
+++ src/tests/tests.test.js
@@ -1,26 +1,4 @@
-import {
-  generateUrlForSimpleRule,
-  matches,
-  generateUrlForAdvancedRuleWithNestedParam,
-} from '../../public/utils/command-utils';
-
-describe('generateUrlWithParameter', () => {
-  it.each([
-    [
-      'abc def',
-      { command: 'abc {{param}}', url: 'https://{{param}}' },
-      'https://def',
-    ],
-    [
-      'abcdef',
-      { command: 'abc{{param}}', url: 'https://{{param}}' },
-      'https://def',
-    ],
-    // ['abcdefxyz', { command: 'abc{{param}}xyz', url: 'https://{{param}}' }, 'https://def'], // doesnt work as of now
-  ])('Works', (typedCommand, rule, expected) => {
-    expect(generateUrlForSimpleRule(typedCommand, rule)).toEqual(expected);
-  });
-});
+import { matches } from '../../public/utils/command-utils';
 
 describe('matches', () => {
   it.each([
@@ -36,29 +14,5 @@ describe('matches', () => {
     ['command ab cd ef', 'comman {{p1}} {{par2}} {{param3}}', false],
   ])('Works for %s', (typedCommand, ruleCommand, expected) => {
     expect(matches(typedCommand, ruleCommand)).toBe(expected);
-  });
-});
-
-describe('generateUrlForAdvancedRuleWithNestedParam', () => {
-  it.each([
-    [
-      'comm ab',
-      {
-        command: 'comm {{dataset.x}}',
-        url: 'https://site/{{dataset.x}}/{{dataset.y}}',
-      },
-      {
-        shortName: 'dataset',
-        values: [
-          { x: 'abc', y: 'def1' },
-          { x: 'def', y: 'def2' },
-        ],
-      },
-      'https://site/abc/def1',
-    ],
-  ])(`Works for %s`, (typedCommand, rule, dataset, expected) => {
-    expect(
-      generateUrlForAdvancedRuleWithNestedParam(typedCommand, rule, dataset)
-    ).toEqual(expected);
   });
 });
