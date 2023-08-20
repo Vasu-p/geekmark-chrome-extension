2415ce48b76dcaf19d515d48e487cbb83950798c
Vasudev Patel <vasudev.patel@sap.com>
2023-08-20 14:15:26 -0700

    add component to show examples

---
+++ src/components/HelpModal/RuleFormatWithExamples.js
@@ -0,0 +1,33 @@
+import { Card, ListGroup } from 'react-bootstrap';
+import { URLInput } from '../URLInput';
+
+// exampleRules is an array of objects with the following properties:
+//   - command
+//   - url
+export function RuleFormatWithExamples({ exampleRules, exampleInteractions }) {
+  return (
+    <Card style={{ marginBottom: '1rem' }}>
+      <Card.Body>
+        <Card.Text>
+          <Card.Title as={'h5'}>Example Rules</Card.Title>
+          <hr />
+          {exampleRules.map((rule, index) => (
+            <URLInput key={index} label={rule.command} value={rule.url} />
+          ))}
+          <Card.Title className="mt-4" as={'h5'}>
+            Example Interactions
+          </Card.Title>
+          <hr />
+          <ListGroup>
+            {exampleInteractions.map((example, index) => (
+              <ListGroup.Item key={index}>
+                <kbd>{example.command}</kbd> →{' '}
+                <span className="link-primary">{example.url}</span>
+              </ListGroup.Item>
+            ))}
+          </ListGroup>
+        </Card.Text>
+      </Card.Body>
+    </Card>
+  );
+}
